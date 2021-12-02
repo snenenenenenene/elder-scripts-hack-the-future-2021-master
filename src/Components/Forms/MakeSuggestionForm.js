e4f0948b1536135775232ec5237f22dd42d0d4a4
snenenenenenene <sennebels@gmail.com>
2021-12-02 15:59:30 +0100

    Suggestion

--- src/Components/Forms/MakeSuggestionForm.js
+++ src/Components/Forms/MakeSuggestionForm.js
@@ -11,10 +11,10 @@ import Button from "../Button";
 
 export const MakeSuggestionForm = ({ gameKey, selectedRoom, newLog }) => {
   const { settings } = useSettings();
-  const [clues, setClues] = useState([])
+  const [clues, setClues] = useState([]);
 
-  const [weapon, setWeapon] = useState(null)
-  const [suspect, setSuspect] = useState(null)
+  const [weapon, setWeapon] = useState(null);
+  const [suspect, setSuspect] = useState(null);
 
   useEffect(() => {
     axios
@@ -30,57 +30,77 @@ export const MakeSuggestionForm = ({ gameKey, selectedRoom, newLog }) => {
       });
   }, []);
 
-
   const handleSubmit = (e) => {
-
     const json = JSON.stringify({
-      "room": selectedRoom,
-      "weapon": weapon,
-      "suspect": suspect
+      room: selectedRoom,
+      weapon: weapon,
+      suspect: suspect,
     });
 
-    axios.post(`https://htf-2021.calibrate.be/api/cluedo/guess?key=${gameKey}`, json,
-    {auth: {
-      username: process.env.REACT_APP_USERNAME,
-      password: process.env.REACT_APP_PASSWORD,
-    },
-       headers: {
-      'Content-Type': 'application/json'
-    }})
-    .then((response) => {
-
-      {
-          console.log(response)
-      }
-
-    }
-    )
+    axios
+      .post(
+        `https://htf-2021.calibrate.be/api/cluedo/guess?key=${gameKey}`,
+        json,
+        {
+          auth: {
+            username: process.env.REACT_APP_USERNAME,
+            password: process.env.REACT_APP_PASSWORD,
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        {
+          console.log(response);
+        }
+      });
     e.preventDefault();
-  }
+  };
 
   const mapSelect = (type) => {
     return clues.map((i) => {
       if (i.type === type) {
-        return (
-          <option value={i.id}>{i.title}</option>
-        )
+        return <option value={i.id}>{i.title}</option>;
       }
-    })
-  }
+    });
+  };
 
   return (
     <div>
       <h2>Maak een suggestie</h2>
-      <form style={{display: "flex", flexDirection: "column", justifyContent: "center", alignContent: "center"}}>
-      <label htmlFor="suspect">Suspect</label>
-        <select id="suspect" name="suspect" type="text" onChange={(e) => setSuspect(e.target.value)}>
-        {mapSelect("suspect")}
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <h2>Room {selectedRoom}</h2>
+        <label htmlFor="suspect">Suspect</label>
+        <select
+          id="suspect"
+          name="suspect"
+          type="text"
+          onChange={(e) => setSuspect(e.target.value)}
+        >
+          {mapSelect("suspect")}
         </select>
         <label htmlFor="weapon">Weapon</label>
-        <select id="weapon" name="weapon" type="text" onChange={(e) => setWeapon(e.target.value)}>
+        <select
+          id="weapon"
+          name="weapon"
+          type="text"
+          onChange={(e) => setWeapon(e.target.value)}
+        >
           {mapSelect("weapon")}
         </select>
-      <Button value="Maak suggestie" onClick={(e) => handleSubmit(e)}></Button>
+        <Button
+          value="Maak suggestie"
+          onClick={(e) => handleSubmit(e)}
+        ></Button>
       </form>
     </div>
   );
